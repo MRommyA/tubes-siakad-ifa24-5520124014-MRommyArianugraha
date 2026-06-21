90a0424cc163e641103580260f97efe7d6036bdc
Rommy <rommyariyanugraha@gmail.com>
2026-06-21 16:57:17 +0700

    add Model

---
+++ SIAKAD/database/migrations/2026_06_21_094335_modify_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->char('npm', 10)->nullable();
+
+            $table->foreign('npm')->references('npm')->on('mahasiswa')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['npm']);
+        });
+    }
+};
