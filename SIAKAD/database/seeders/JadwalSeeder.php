7d3f2f8c4454161802c5c9bbaf7c11bd9f7d8ce9
Rommy <rommyariyanugraha@gmail.com>
2026-06-23 12:06:35 +0700

    add crud controller dan routes

--- SIAKAD/database/seeders/JadwalSeeder.php
+++ SIAKAD/database/seeders/JadwalSeeder.php
@@ -14,9 +14,9 @@ class JadwalSeeder extends Seeder
     public function run(): void
     {
         $listJadwal = [
-            ['kode_matakuliah' => 'IF350122', 'nidn' => '0001234501', 'kelas' => 'A', 'hari' => 'Senin', 'jam' => '2026-06-22 08:00:00'],
-            ['kode_matakuliah' => 'IF451223', 'nidn' => '0001234502', 'kelas' => 'B', 'hari' => 'Rabu', 'jam' => '2026-06-24 10:30:00'],
-            ['kode_matakuliah' => 'IF460223', 'nidn' => '0001234503', 'kelas' => 'A', 'hari' => 'Sabtu', 'jam' => '2026-06-27 13:00:00']
+            ['kode_matakuliah' => 'IF350122', 'nidn' => '0001234501', 'kelas' => 'A', 'hari' => 'Senin', 'jam' => '2026-06-30 08:00:00'],
+            ['kode_matakuliah' => 'IF451223', 'nidn' => '0001234502', 'kelas' => 'B', 'hari' => 'Rabu', 'jam' => '2026-06-30 10:30:00'],
+            ['kode_matakuliah' => 'IF460223', 'nidn' => '0001234503', 'kelas' => 'A', 'hari' => 'Sabtu', 'jam' => '2026-06-30 13:00:00']
         ];
 
         foreach($listJadwal as $jadwal){
